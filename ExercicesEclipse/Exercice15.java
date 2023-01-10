99d0d01903649535e1dd68abcd9dffa0ddb6544c
59013-42-15 <59013-42-15@exchange.ad.afpanet>
2023-01-10 16:19:38 +0100

    ajout exercices eclipse

---
+++ ExercicesEclipse/Exercice15.java
@@ -0,0 +1,23 @@
+package projet1;
+import java.util.Scanner;
+public class Exercice15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		//Ecrire un programme en java qui demande un nombre compris entre 10 et 20, jusqu’à ce que la réponse
+		//convienne. En cas de réponse supérieure à 20, on fera apparaître un message : « Plus petit ! », et
+	//	inversement, « Plus grand ! » si le nombre est inférieur à 10.
+		
+		Scanner scan = new Scanner(System.in);
+
+		// Je declare mes variables de type entier
+		
+		
+		
+		
+	}
+
+}
